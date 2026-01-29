704834ffe53e26ef5c2af376d8ef7209e2d8d91f
MizanRbf <mizanrbf@gmail.com>
2026-01-29 22:33:02 +0600

    nodemailer installed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
+// Send Email Setup
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+  },
+});
 export const auth = betterAuth({
   // App info
   database: prismaAdapter(prisma, {
