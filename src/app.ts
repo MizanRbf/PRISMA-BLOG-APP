85862863b7888fc77e77da3a42e79a99297d49f7
MizanRbf <mizanrbf@gmail.com>
2026-01-29 22:05:30 +0600

    email verification settings added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
-import express from "express";
+import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.route";
-const app = express();
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
+const app: Application = express();
+
+// Cors
+app.use(
+  cors({
+    origin: process.env.App_URL || "http://localhost:4000",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
-// post routes
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,41 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+  // App info
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  // Security
+  trustedOrigins: [process.env.APP_URL!],
+  // Custom user fields
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "user",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "active",
+        required: false,
+      },
+    },
+  },
+  // Authentication methods
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  // Email verification
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("********send verification email");
+    },
+  },
 });
