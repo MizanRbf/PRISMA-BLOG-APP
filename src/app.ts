b021e66efd5a5ea98e65747a8306d13d9f9846f3
MizanRbf <mizanrbf@gmail.com>
2026-01-29 18:46:17 +0600

    used express.json middleware to solve problem about posting data

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { postRouter } from "./modules/post/post.route";
 const app = express();
 
+app.use(express.json());
+
 // post routes
 app.use("/posts", postRouter);
 
