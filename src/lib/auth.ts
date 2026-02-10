6b16ee7be11e9a4ed6f4fa3f68fb0db274cd1c33
MizanRbf <mizanrbf@gmail.com>
2026-02-10 17:48:41 +0600

    google client id and secret added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -47,28 +47,33 @@ export const auth = betterAuth({
   },
   // Email verification
   emailVerification: {
+    sendOnSignUp: true,
     sendVerificationEmail: async ({ user, url, token }, request) => {
-      const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
-      const info = await transporter.sendMail({
-        from: '"Prisma Blog" <prismablog@ph.com>',
-        to: user.email,
-        subject: "Hello ✔",
-        text: "Hello world?",
-        html: `
+      try {
+        const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
+        const info = await transporter.sendMail({
+          from: '"Prisma Blog" <prismablog@ph.com>',
+          to: user.email,
+          subject: "Please verify your email address",
+          text:
+            "Please verify your email address by clicking the link:" +
+            verificationUrl,
+          // HTML email content
+          html: `
     <div style="font-family: Arial, sans-serif; background-color: #f4f6f8; padding: 40px;">
       <div style="max-width: 520px; margin: auto; background: #ffffff; padding: 30px; border-radius: 8px;">
         
         <h2 style="color: #333;">Verify your email</h2>
-
+       
         <p style="color: #555; font-size: 15px;">
           Hi <strong>${user.name}</strong>,
         </p>
-
+      
         <p style="color: #555; font-size: 15px;">
           Thanks for signing up for <strong>Prisma Blog</strong>.
           Please confirm your email address by clicking the button below.
         </p>
-
+       
         <div style="text-align: center; margin: 30px 0;">
           <a href="${verificationUrl}"
              style="
@@ -83,22 +88,26 @@ export const auth = betterAuth({
             Verify Email
           </a>
         </div>
-
+         
         <p style="color: #777; font-size: 13px;">
           If you did not create an account, you can safely ignore this email.
         </p>
 
         <hr style="margin: 30px 0;" />
-
+              
         <p style="color: #999; font-size: 12px; text-align: center;">
           © ${new Date().getFullYear()} Prisma Blog. All rights reserved.
         </p>
       </div>
     </div>
   `,
-      });
+        });
 
-      console.log("Message sent:", info.messageId);
+        console.log("Message sent:", info.messageId);
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
   },
 });
