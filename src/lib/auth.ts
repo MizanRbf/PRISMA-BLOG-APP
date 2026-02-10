373af0f3dcdbc788ff9e17574280013b437cc6a5
MizanRbf <mizanrbf@gmail.com>
2026-02-10 17:59:33 +0600

    google providers added in auth ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -110,4 +110,11 @@ export const auth = betterAuth({
       }
     },
   },
+  // Google OAuth
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
