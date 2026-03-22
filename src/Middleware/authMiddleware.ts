7b580c5ef44b4535d8205ab8a773467b2bc28977
MizanRbf <mizanrbf@gmail.com>
2026-03-22 06:06:28 +0600

    authMiddleware error solved

--- src/Middleware/authMiddleware.ts
+++ src/Middleware/authMiddleware.ts
@@ -1,8 +1,48 @@
 import { NextFunction, Request, Response } from "express";
+import { auth as betterAuth } from "../lib/auth";
 
-export default function auth(...roles: any) {
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+export default function auth(...roles: string[]) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    console.log(roles);
+    // get user session
+    const session = await betterAuth.api.getSession({
+      headers: req.headers as any,
+    });
+
+    if (!session) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorized",
+      });
+    }
+
+    if (!session.user.emailVerified) {
+      return res.status(403).json({
+        success: false,
+        message: "Please verify your email address",
+      });
+    }
+
+    req.user = {
+      id: session.user.id,
+      email: session.user.email,
+      name: session.user.name,
+      role: session.user.role as string,
+      emailVerified: session.user.emailVerified,
+    };
+
     next();
   };
 }
