187a084be7f6034a7b8ae9f608ff3cbac8bcf8bd
MizanRbf <mizanrbf@gmail.com>
2026-03-27 06:44:18 +0600

    checkup postId and parentId

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -7,10 +7,28 @@ const createComment = async (payload: {
   postId: string;
   parentId?: string;
 }) => {
+  // Check if the post exists
+  const postData = await prisma.post.findUnique({
+    where: {
+      id: payload.postId,
+    },
+  });
+  if (!postData) {
+    throw new Error("Post not found!! maybe post id is wrong!!!");
+  }
+  // Check if the parent comment exists
+  if (payload.parentId) {
+    await prisma.comments.findUniqueOrThrow({
+      where: {
+        id: payload.parentId,
+      },
+    });
+  }
   return await prisma.comments.create({
     data: payload,
   });
 };
+
 // Export comment service
 export const commentService = {
   createComment,
